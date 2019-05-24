31e49fb3652fd83bf55d3f30de766a71dc894e01
AsbjornOlling <asbjornolling@gmail.com>
2019-05-24 15:52:39 +0200

    bunch of stuff related to managing lists of voted songs

--- justify/templates/newuser.tpl
+++ justify/templates/newuser.tpl
@@ -3,9 +3,39 @@
 {% block content %}
 	<div class="container">
 		
-		<form action="/newuser" method="POST" class="input-group">
-			<label for="username">Welcome! Please tell me your name.</label>
+		<img src="{{ url_for('static', filename='j256.png') }}" alt="logo" class="img-responsive mx-auto d-block" />
 
+		<br>
+
+		<br>
+
+		<h4 class="list-group-item-heading">Add whatever music you want!</h4>
+		<p style="font-size: 16px;" class="list-group-item-text">
+			Search for any song. Results come from spotify, soundcloud, and youtube. Odds are you'll find what you're looking for.
+		</p>
+
+		<br>
+
+		<h4 class="list-group-item-heading">No selfish speaker-hijacks. Ever.</h4>
+		<p style="font-size: 16px;" class="list-group-item-text">
+			The next song will always be the one in highest demand. 
+			It's very simple; You leave a vote on the songs you want to hear, and the playlist sorts itself automagically.
+		</p>
+
+		<br>
+
+		<h4 class="list-group-item-heading">Wow, that's fucking cool!</h4>
+		<p style="font-size: 16px;" class="list-group-item-text">
+			Right? Even better, it's 100% open source software! It was made for Roskilde Festival 2017 by Asbjørn, a member of Camp C.A.M.P.. 
+			If you want to know more, go get Asbjørn a beer.
+		</p>
+
+		<br>
+
+		<br>
+
+		<h5 class="text-center">Tell me your name to get started!</h5>
+		<form action="{{ url_for('web.new_user') }}" method="POST" class="input-group">
 			<!-- username field -->
 			<input type="text" id="username" name="username" class="form-control" placeholder="Your Name">
 
--- justify/users.py
+++ justify/users.py
@@ -1,20 +1,12 @@
 """
 Manages registered users in Redis.
-
-Redis schema:
-
-justify:user:uniqueid
-{
-    'username':     str,
-    'votes_current': list[str],
-    'votes_history': list[str]
-}
-
 """
 
 # std lib
-import functools
+import json
+from functools import wraps
 from uuid import uuid4
+from typing import List
 
 # deps
 from loguru import logger
@@ -29,10 +21,8 @@ REDIS_USER_PREFIX = 'justify:user:'
 def check_user(f):
     """ Decorator: redirects user to new_user page
     if the user is unknown.
-    XXX: This is buggy
-    TODO: fix this shit
     """
-    @functools.wraps(f)
+    @wraps(f)
     def decorated_f(*args, **kwargs):
         logger.debug(f"SESSION IN DEC: {session}")
         if 'userid' not in session:
@@ -64,3 +54,47 @@ def add_user(username: str):
     r.hmset(f'{REDIS_USER_PREFIX}{userid}', udata)
 
     return userid
+
+
+def user_voted(songuri: str, uid=None):
+    """ Add URI to list of songs voted on, for given userid. """
+    if uid is None:
+        userid = session['userid']
+
+    # get current user data from redis
+    r = get_redis()
+    rkey = f'{REDIS_USER_PREFIX}{userid}'
+    udata = r.hmget(rkey, 'votes_current', 'votes_history')
+
+    # append song uri to json lists of voted songs
+    def appenduri(arraystr: str) -> str:
+        """ Safely append song uri to a json array of strings. """
+        array = json.loads(arraystr)
+        return json.dumps(array.append(songuri))
+
+    udata['votes_current'] = appenduri(udata['votes_current'])
+    udata['votes_history'] = appenduri(udata['votes_history'])
+
+    # put the new data into redis
+    r.hmset(rkey, udata)
+    logger.debug(f"Updated votelist of {udata['userdata']}")
+
+
+def clear_uservotes(songuri: str):
+    """ Remove uri from all users' 'votes_current' lists. """
+    # get all redis keys
+    r = get_redis()
+    userids = r.scan_iter(f"{REDIS_USER_PREFIX}*")
+
+    for uid in userids:
+        # get 'votes_current' field from user entry
+        vcurr = r.hmget(uid, 'votes_current')[0]
+        assert isinstance(vcurr, bytes)
+
+        if str(songuri) in str(vcurr):
+            # if user voted on song, remove the vote
+            logger.info(f"Clearing vote {songuri} for user {uid}")
+            currlist = json.loads(vcurr[0])
+            newvcurr = json.dumps(currlist.remove(songuri))
+            assert len(newvcurr) < len(vcurr)
+            r.hmset(uid, {'votes_current': newvcurr})
