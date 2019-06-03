6c512420a0195ffc5ce42ec9b27f6762d96d3f93
AsbjornOlling <asbjornolling@gmail.com>
2019-06-03 17:24:47 +0200

    fixed sorting bug, deleted unused template

--- justify/mopidy_connection.py
+++ justify/mopidy_connection.py
@@ -41,19 +41,19 @@ def track_playback_ended(event):
 
 
 @mp.on_event('options_changed')
-def fix_mopidy_options():
+def fix_mopidy_options(event):
     """ Sets the necessary playback options in Mopidy.
     Since setting an option triggers the 'options_changed'
     event, we have to check the option first, to avoid an
     infinite loop.
     """
-    # obviously shuffle won't make sense
-    if mp.tracklist.get_random() is not False:
-        mp.tracklist.set_random(False)
-
     # remove track from playlist after playing
     if mp.tracklist.get_consume() is not True:
         mp.tracklist.set_consume(True)
+
+    # obviously shuffle won't make sense
+    if mp.tracklist.get_random() is not False:
+        mp.tracklist.set_random(False)
 
     # dont loop tracks
     if mp.tracklist.get_repeat() is not False:
@@ -100,20 +100,28 @@ def sort_mopidy():
     This could involve quite a lot of Mopidy calls.
     XXX: this is likely not the most clean / efficient
          implementation possible.
+    TODO: this is weirdly buggy. sorts in reverse order
     """
     logger.debug("Sorting Mopidy...")
 
-    vlist: List[str] = get_votelist()        # list of uris in vote order
-    tllist = mp.tracklist.get_tl_tracks()    # TlTracks in playlist order
-    tlsorted = sorted(tllist, reverse=True,  # TlTracks in vote order
+    vlist: List[str] = get_votelist()            # list of uris in vote order
+    tllist: list = mp.tracklist.get_tl_tracks()  # TlTracks in playlist order
+
+    # remove currently playing track from both lists
+    vlist.remove(tllist[0].track.uri)
+    del tllist[0]
+
+    # sort TlTracks in desc. vote order (desired state)
+    tlsorted = sorted(tllist, reverse=True,
                       key=lambda t: vlist.index(t.track.uri))
 
     if [str(t.track.uri) for t in tllist] == vlist:
+        # abort if no change in order
         logger.debug("Songs already in order. Aborting sort.")
         return
 
     # move all tracks into place (except currently playing)
-    for dst, tltrack in list(enumerate(tlsorted))[1:]:
+    for dst, tltrack in enumerate(tlsorted, start=1):
         src = mp.tracklist.index(tl_track=tltrack)
         if src != dst:
             # move track to destination spot on tracklist
@@ -124,5 +132,7 @@ def sort_mopidy():
     finalorder = [str(t.uri) for t in mp.tracklist.get_tracks()]
     if finalorder != vlist:
         # run again, if sort failed
-        logger.error("Sort result unsuccessful. Running again...")
-        sort_mopidy()
+        logger.error("Sort result unsuccessful. (( NOT )) Running again...")
+        logger.debug(f"Final: {finalorder}")
+        logger.debug(f"Vlist: {vlist}")
+        # sort_mopidy()
