852f92abf873e188d0670474f8ae76765296de32
AsbjornOlling <asbjornolling@gmail.com>
2019-06-17 02:15:09 +0200

    wait for mopidy to get up before doing anythin

--- justify/__init__.py
+++ justify/__init__.py
@@ -5,9 +5,25 @@ from time import sleep
 # deps
 from loguru import logger
 from flask import Flask
+from requests import get
 
 # app imports
 from . import config
+
+
+def wait_for_mopidy(mopidyhost):
+    """ Send HTTP to mopidy until it responds """
+    # wait for mopidy to come up
+    while True:
+        try:
+            r = get(f"http://{mopidyhost}")
+            if r.status_code == 200:
+                break
+        except Exception:
+            logger.warning(f"Mopidy seems down at address: {mopidyhost}"
+                           " Trying to connect...")
+            sleep(2)
+    logger.debug("Mopidy seems to be up.")
 
 
 def create_app() -> Flask:
@@ -18,7 +34,7 @@ def create_app() -> Flask:
     # make flask app
     app = Flask(__name__, static_url_path='/static')
 
-    # read configuration into flask config obj
+    # read config into flask from env and file
     conf = config.load_config()
     app.config.update(**conf)
 
@@ -28,16 +44,18 @@ def create_app() -> Flask:
     }
     app.config.update(**sessconf)
 
+    # wait for mopidy to come up
+    # (b/c the mopidy docker image is a liar)
+    wait_for_mopidy(conf['MOPIDY_HOST'])
+    with app.app_context():
+        # set mopidy options right
+        from .mopidy_connection import fix_mopidy_options
+        fix_mopidy_options(None)
+
     # register web endpoints
     with app.app_context():
         from . import views
     app.register_blueprint(views.bp)
-
-    # fix options on setup
-    with app.app_context():
-        sleep(5)  # this sleep is shit, but necessary because the mopidy container is a liar
-        from .mopidy_connection import fix_mopidy_options
-        fix_mopidy_options(None)
 
     return app
 
